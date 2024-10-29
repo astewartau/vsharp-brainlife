9f9b2f16a25f50b22eaac00ae50ad3a0c10af1f8
Ashley Stewart <a.stewart.au@gmail.com>
2024-10-29 16:07:59 -0500

    Update parameter names

--- main.jl
+++ main.jl
@@ -37,7 +37,7 @@ function main()
 	config_data = load_config("config.json")
 		
 	println("[INFO] Extracting information...")
-	frequency_path = config_data["frequency"]
+	fieldmap_path = config_data["fieldmap"]
 	mask_path = config_data["mask"]
 	input_units = config_data["input_units"]
 	output_units = config_data["output_units"]
@@ -46,7 +46,7 @@ function main()
 
     println("[INFO] Checking files exist...")
     all_files_exist = true
-    for path in [frequency_path, mask_path]
+    for path in [fieldmap_path, mask_path]
         if !isfile(path)
             println("Error: File not found: $path")
             all_files_exist = false
@@ -59,33 +59,33 @@ function main()
     end
 
     println("[INFO] Loading NIfTI images...")
-    frequency_nii = niread(frequency_path)
+    fieldmap_nii = niread(fieldmap_path)
     
-    vsz = voxel_size_safe(frequency_nii.header)
+    vsz = voxel_size_safe(fieldmap_nii.header)
     println("[INFO] Read voxel size ", vsz)
 
     mask_nii = niread(mask_path)
     mask = !=(0).(mask_nii.raw)
-    frequency = frequency_nii.raw .* mask
+    fieldmap = fieldmap_nii.raw .* mask
 
     println("[INFO] Converting units to Hz...")
     if input_units == "rad/s"
-        frequency = frequency ./ (2π)
+        fieldmap = fieldmap ./ (2π)
     elseif input_units == "radians"
         γ = 267.52e6 # rad/s/T
-        @views for t in axes(frequency, 4)
-            frequency[:, :, :, t] .*= inv(B0 * γ * TE)
+        @views for t in axes(fieldmap, 4)
+            fieldmap[:, :, :, t] .*= inv(B0 * γ * TE)
         end
     end
 
     println("[INFO] Performing V-SHARP background field removal...")
-    tissue_phase, vsharp_mask = vsharp(frequency, mask, vsz)
+    tissue_phase, vsharp_mask = vsharp(fieldmap, mask, vsz)
 
     println("[INFO] Saving outputs...")
     mkpath("tissue_freq")
-    savenii(tissue_phase, "t2starw.nii.gz", "tissue_freq", frequency_nii.header)
+    savenii(tissue_phase, "t2starw.nii.gz", "tissue_freq", fieldmap_nii.header)
     mkpath("mask")
-    savenii(vsharp_mask, "mask.nii.gz", "mask", frequency_nii.header)
+    savenii(vsharp_mask, "mask.nii.gz", "mask", fieldmap_nii.header)
 
     println("[INFO] V-SHARP processing completed successfully.")
 end
